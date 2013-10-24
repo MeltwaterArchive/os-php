c78bc088fc26e070ab11e579e89edf8b0c7bb955
Rick Ogden <rick@rickogden.com>
2013-10-24 11:45:37 +0100

    added Mavericks class to Darwin

---
+++ src/Datasift/Os/Darwin/Mavericks.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Datasift\Os\Darwin;
+
+use Datasift\Os\Darwin;
+
+/**
+ * Mavericks
+ *
+ * @author Rick Ogden <rick@rickogden.com>
+ */
+class Mavericks extends Darwin {
+
+    /**
+     * The major + minor version of the OS
+     */
+    const VERSION = "13.0";
+
+}
